b4630ceb11eddcd578a220ba5a3c4e43eedb3e4e
Evandro Carreira <evandro.carreira@gmail.com>
2021-06-21 09:17:37 -0300

    feat(quadrinhos) primeiras imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import img1 from './quadrinhos/1.1.png'
+import img2 from './quadrinhos/1.2+3.png'
+import img3 from './quadrinhos/1.4+5+6.png'
+import img4 from './quadrinhos/2.1+2.png'
+import img5 from './quadrinhos/2.3+4.png'
+import img6 from './quadrinhos/2.5+6.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quadrinhos-container">
+      <img src={img1} alt='1.1'/>
+      <img src={img2} alt='1.2'/>
+      <img src={img3} alt='1.4'/>
+      <img src={img4} alt='2.1'/>
+      <img src={img5} alt='2.3'/>
+      <img src={img6} alt='2.5'/>
     </div>
   );
 }
